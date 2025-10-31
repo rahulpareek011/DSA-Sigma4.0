e5ec34937d74a8ae96e86edd18a0805447748d00
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-10-31 11:12:43 +0530

    Printing number in descending order using recursion in java

--- Recursion/PrintNumDecOrder.java
+++ Recursion/PrintNumDecOrder.java
@@ -1,14 +1,14 @@
 public class PrintNumDecOrder {
     public static void decNum(int n){
-        if(n == 1){
+        if(n == 1){ //base condition to return and avoid stackoverflowerror
             System.out.print(n);
             return;
         }
-        System.out.print(n+" ");
-        decNum(n-1);
+        System.out.print(n+" ");//before minus printing value
+        decNum(n-1); //pass 9 and then 8..7 ..6......1
     }
     public static void main(String[] args) {
         int n = 10;
-        decNum(n);
+        decNum(n);//passing 10 decNum(n)
     }
 }
