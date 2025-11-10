219bee6eafc9da851ee9946ae21d2c70176b6325
rahul pareek <itsrahul615@gmail.com>
2025-11-10 21:14:49 +0530

    Your commit message

---
+++ Recursion/PrintNumIncOrder.java
@@ -0,0 +1,13 @@
+public class PrintNumIncOrder {
+    public static void printInc(int n){
+        if(n==1){
+            System.out.println(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n+ " ");
+    }
+    public static void main(String[] args) {
+        printInc(5);
+    }
+}
